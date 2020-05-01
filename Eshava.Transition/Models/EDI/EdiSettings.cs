474be50e7e95fc6f69b1b0512ebc0f517e453e0d
Tino Krüger <krueger.tino@googlemail.com>
2020-05-02 00:00:56 +0200

    Fixed compiler warning

--- Eshava.Transition/Models/EDI/EdiSettings.cs
+++ Eshava.Transition/Models/EDI/EdiSettings.cs
@@ -1,11 +1,8 @@
-﻿using System.Globalization;
-
-namespace Eshava.Transition.Models.EDI
+﻿namespace Eshava.Transition.Models.EDI
 {
 	public class EdiSettings : AbstractSettings
 	{
 		public string[] DataRows { get; set; }
 		public int CurrentStartRowIndex { get; set; }
-		public CultureInfo CultureInfo { get; set; }
 	}
 }
